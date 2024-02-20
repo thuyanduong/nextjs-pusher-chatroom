cbb24998d5f7bac06efd1cdca03f2d98a49caf52
thuyanduong <thuyanannduong@gmail.com>
2024-02-20 12:45:12 -0500

    removed lint errors

--- app/lib/context/stateProvider.js
+++ app/lib/context/stateProvider.js
@@ -70,7 +70,7 @@ export default function StateProvider(props) {
       });
       setJoinedChannel(false);
     }
-  }, [currChannel]);
+  }, [currChannel, joinedChannel, channels]);
 
   const properties = {
     user,
